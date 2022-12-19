44296a68840654de86754d97898f6b1448aebf29
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2022-12-19 18:06:01 -0300

    Sistema de contagem de animais

---
+++ AnimalDeEstimacao/AnimalDeEstimacao/Animal.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalDeEstimacao
+{
+    public class Animal
+    {
+		private String nome;
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+		private String tipo;
+
+		public String Tipo
+		{
+			get { return tipo; }
+			set 
+			{ 
+				if(value == "Cachorro" || value == "Gato" || value == "Peixe") 
+				{
+					tipo = value;
+				} else
+				{
+					tipo = "Peixe";
+				}
+			}
+		}
+
+		public void ExibirAnimal()
+		{
+			Console.WriteLine("Nome: " + this.Nome);
+			Console.WriteLine("Tipo: " + this.Tipo);
+		}
+
+		
+    }
+}
---
+++ AnimalDeEstimacao/AnimalDeEstimacao/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalDeEstimacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new Animal();
+            Animal animal2 = new Animal();
+            Animal animal3 = new Animal();
+            Animal animal4 = new Animal();
+            Animal animal5 = new Animal();
+
+            int qtdCachorro = 0, qtdGato = 0, qtdPeixe = 0;
+
+            Console.WriteLine("Cadastro de Animais de Estimação");
+
+            Console.Write("Nome: ");
+            animal1.Nome = Console.ReadLine();
+            Console.WriteLine("Tipos:\nCachorro\nGato\nPeixe");
+            Console.Write("Tipo: ");
+            animal1.Tipo = Console.ReadLine();
+            if(animal1.Tipo == "Cachorro")
+            {
+                qtdCachorro++;
+            } else if(animal1.Tipo == "Gato") 
+            {
+                qtdGato++;
+            } else { 
+                qtdPeixe++;
+            }
+
+            Console.Write("Nome: ");
+            animal2.Nome = Console.ReadLine();
+            Console.WriteLine("Tipos:\nCachorro\nGato\nPeixe");
+            Console.Write("Tipo: ");
+            animal2.Tipo = Console.ReadLine();
+            if (animal2.Tipo == "Cachorro")
+            {
+                qtdCachorro++;
+            }
+            else if (animal2.Tipo == "Gato")
+            {
+                qtdGato++;
+            }
+            else
+            {
+                qtdPeixe++;
+            }
+
+            Console.Write("Nome: ");
+            animal3.Nome = Console.ReadLine();
+            Console.WriteLine("Tipos:\nCachorro\nGato\nPeixe");
+            Console.Write("Tipo: ");
+            animal3.Tipo = Console.ReadLine();
+            if (animal3.Tipo == "Cachorro")
+            {
+                qtdCachorro++;
+            }
+            else if (animal3.Tipo == "Gato")
+            {
+                qtdGato++;
+            }
+            else
+            {
+                qtdPeixe++;
+            }
+
+            Console.Write("Nome: ");
+            animal4.Nome = Console.ReadLine();
+            Console.WriteLine("Tipos:\nCachorro\nGato\nPeixe");
+            Console.Write("Tipo: ");
+            animal4.Tipo = Console.ReadLine();
+            if (animal4.Tipo == "Cachorro")
+            {
+                qtdCachorro++;
+            }
+            else if (animal4.Tipo == "Gato")
+            {
+                qtdGato++;
+            }
+            else
+            {
+                qtdPeixe++;
+            }
+
+            Console.Write("Nome: ");
+            animal5.Nome = Console.ReadLine();
+            Console.WriteLine("Tipos:\nCachorro\nGato\nPeixe");
+            Console.Write("Tipo: ");
+            animal5.Tipo = Console.ReadLine();
+            if (animal5.Tipo == "Cachorro")
+            {
+                qtdCachorro++;
+            }
+            else if (animal5.Tipo == "Gato")
+            {
+                qtdGato++;
+            }
+            else
+            {
+                qtdPeixe++;
+            }
+
+            Console.WriteLine("----------------------------------------");
+            animal1.ExibirAnimal();
+            animal2.ExibirAnimal();
+            animal3.ExibirAnimal();
+            animal4.ExibirAnimal();
+            animal5.ExibirAnimal();
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Ao todo, foram cadastrados " + qtdCachorro + " cachorros, " + 
+                qtdGato + " gatos e " + qtdPeixe + " peixes.");
+        }
+    }
+}
