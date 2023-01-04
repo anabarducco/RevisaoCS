7c36a3289bfa99bba03adf69144df554b47165cd
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2023-01-04 16:58:17 -0300

    Exercício Cálculo de Área de um Quadrilátero

---
+++ CalculoArea/CalculoArea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa de Cálculo de Área de Quadrilátero");
+            Console.WriteLine();
+
+            Console.Write("Valor da base: ");
+            int valorBase = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Valor da altura: ");
+            int valorAltura = Convert.ToInt32(Console.ReadLine());
+            Quadrilatero quad = new Quadrilatero(valorBase, valorAltura);
+
+            Console.WriteLine("------------------------------");
+            quad.ExibirDados();
+        }
+    }
+}
---
+++ CalculoArea/CalculoArea/Quadrilatero.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoArea
+{
+    public class Quadrilatero
+    {
+        public Quadrilatero() { }
+        public Quadrilatero(int baseQuadrilatero, int alturaQuadrilatero)
+        {
+            this.BaseQuad = baseQuadrilatero;
+            this.AltQuad= alturaQuadrilatero;
+        }
+
+        private int baseQuad;
+
+        public int BaseQuad
+        {
+            get { return baseQuad; }
+            set 
+            { 
+                if (value >= 0) baseQuad = value; 
+                else baseQuad = 0;
+            }
+        }
+
+        private int altQuad;
+
+        public int AltQuad
+        {
+            get { return altQuad; }
+            set 
+            {
+                if (value >= 0) altQuad = value;
+                else altQuad = 0;
+            }
+        }
+
+        public int Area
+        {
+            get 
+            {
+                return baseQuad * altQuad;
+            }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Valores do Quadrilátero:");
+            Console.WriteLine("Base = " + this.BaseQuad);
+            Console.WriteLine("Altura = " + this.AltQuad);
+            Console.WriteLine("Área = " + this.Area);
+        }
+    }
+}
