ca15db6edb0e3fb3457129faf02631039449a41a
Ana Beatriz Barducco <48134088+anabarducco@users.noreply.github.com>
2023-01-04 11:08:25 -0300

    Projeto Cálculo Maior Idade entre Pessoas Cadastradas

---
+++ MaiorIdade/MaiorIdade/Pessoa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorIdade
+{
+    public class Pessoa
+    {
+		public Pessoa() { }
+		public Pessoa(String nomePessoa, int idadePessoa) 
+		{
+			this.Nome = nomePessoa;
+			this.Idade = idadePessoa;
+		}
+
+		private String nome;
+
+		public String Nome
+		{
+			get { return nome; }
+			set { nome = value.ToUpper(); }
+		}
+
+		private int idade;
+
+		public int Idade
+		{
+			get { return idade; }
+			set { idade = value; }
+		}
+
+		public void ExibirDados()
+		{
+			Console.WriteLine("Nome: " + this.Nome);
+			Console.WriteLine("Idade: " + this.Idade);
+		}
+	}
+}
---
+++ MaiorIdade/MaiorIdade/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaiorIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cadastro de Pessoas");
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Pessoa 1"); 
+            Console.Write("Nome: ");
+            String nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Pessoa p1 = new Pessoa(nome, idade);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Pessoa 2");
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Pessoa p2 = new Pessoa(nome, idade);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Pessoa 3");
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade = Convert.ToInt32(Console.ReadLine());
+            Pessoa p3 = new Pessoa(nome, idade);
+
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("As pessoas cadastradas foram: ");
+            p1.ExibirDados();
+            p2.ExibirDados();
+            p3.ExibirDados();
+            if ((p1.Idade > p2.Idade) && (p1.Idade > p3.Idade))
+            {
+                Console.WriteLine("A maior idade cadastrada foi da pessoa " + p1.Nome + " com " + p1.Idade + " anos.");
+            } else if ((p2.Idade > p1.Idade) && (p2.Idade > p3.Idade))
+            {
+                Console.WriteLine("A maior idade cadastrada foi da pessoa " + p2.Nome + " com " + p2.Idade + " anos.");
+            } else if ((p3.Idade > p1.Idade) && (p3.Idade > p2.Idade))
+            {
+                Console.WriteLine("A maior idade cadastrada foi da pessoa " + p3.Nome + " com " + p3.Idade + " anos.");
+            } else
+            {
+                Console.WriteLine("Todas as pessoas cadastradas têm a mesma idade.");
+            }
+            
+        }
+    }
+}
